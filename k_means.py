8f37d70c72e0a9450a2f9af8dbba623bd0c3355c
talhaahussain <talhaahussain@gmail.com>
2023-08-07 16:50:32 +0100

    Formatted imported CSV data
    Identified desired columns
    Removed NULL/empty rows
    Scaled data

--- k_means.py
+++ k_means.py
@@ -2,3 +2,11 @@ import pandas as pd
 import numpy as np
 
 players = pd.read_csv("all_seasons.csv")
+
+features = ["player_height", "player_weight", "draft_year", "draft_round", "draft_number"]
+players = players.dropna(subset=features) # Removes NULL/missing values
+data = players[features].copy()
+
+
+data = ((data - data.min()) / (data.max() - data.min())) * 99 + 1 # Data scaling
+print(data.describe())
