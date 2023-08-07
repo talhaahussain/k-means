48b9ec680bf623bee7481ea32dcf9b8d82d813d6
talhaahussain <talhaahussain@gmail.com>
2023-08-07 17:02:18 +0100

    Added initialisation of random centroids
    Wrapped data initialistion and centroid initialisation into functions
    One warning thrown from calling float on a single element Series

--- k_means.py
+++ k_means.py
@@ -1,12 +1,22 @@
 import pandas as pd
 import numpy as np
 
-players = pd.read_csv("all_seasons.csv")
-
+file = "all_seasons.csv"
 features = ["player_height", "player_weight", "draft_year", "draft_round", "draft_number"]
-players = players.dropna(subset=features) # Removes NULL/missing values
-data = players[features].copy()
+ 
+def initialise_data(file, features):
+    players = pd.read_csv(file)
+    players = players.dropna(subset=features) # Removes NULL/missing values
+    data = players[features].copy()
+    data = ((data - data.min()) / (data.max() - data.min())) * 99 + 1 # Data scaling
+    return data
 
+def initialise_centroids(data, k):
+    centroids = []
+    for i in range(k): 
+        centroid = data.apply(lambda x: float(x.sample()))
+        centroids.append(centroid)
+    return pd.concat(centroids, axis=1)
 
-data = ((data - data.min()) / (data.max() - data.min())) * 99 + 1 # Data scaling
-print(data.describe())
+data = initialise_data(file, features)
+centroids = initialise_centroids(data, 5)
